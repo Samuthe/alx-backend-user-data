43487a44bd1297c38c2836adb61f9e381f891ec7
Samuthe <ekatisamuel@gmail.com>
2023-03-23 12:51:30 +0300

    filtered data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,9 +10,9 @@ separating all fields in the log line (message)
 
 from typing import List
 import re
-# import logging
-# import mysql.connector
-# from os import environ
+import logging
+import mysql.connector
+from os import environ
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -26,10 +26,15 @@ def filter_datum(fields: List[str], redaction: str,
                          f'{f}={redaction}{separator}', message)
     return message
 
-# def filter_datum(fields: List[str], redaction: str,
-#                  message: str, separator: str) -> str:
-#     '''returns obfuscated message'''
-#     for x in fields:
-#         message = re.sub(f'{x}= .*?{separator}',
-#                          f'{x}={redaction}{separator}', message)
-#     return message
+
+def get_logger() -> logging.Logger:
+    """ Returns a Logger Object """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
